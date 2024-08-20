d7d3d9eaa7da7673784f9a6ad19a21297e44a72d
braboyb <braboyb1@gmail.com>
2024-08-20 13:50:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'S6zIpeIIXjY9fvM', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 
 //language 
@@ -20,7 +20,7 @@ global.language = indonesia //change indonesia to english if you don't understan
 global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "Alphabot-Mdོ" //your bot name
 global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
+global.OWNER_NUMBER = "628996586515" //your telegram number
 global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
